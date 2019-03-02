38eee27a6a52e9f54214e8222345be01fede7b9a
Rob-Marshall87 <marshallrobbie@hotmail.com>
2019-03-02 20:01:05 +0000

    all MVP completed, sell_pet_to_customer needs completed

--- pet_shop.rb
+++ pet_shop.rb
@@ -41,16 +41,17 @@ end
 def find_pet_by_name(pet_shop, pet_name)
   for pet in pet_shop[:pets]
     if pet[:name] == pet_name
-      return pet_name
-    else
-      return nil
+      return pet
     end
   end
+  return nil
 end
 
 def remove_pet_by_name(pet_shop, name)
   for pet in pet_shop[:pets]
-    pet[:name].delete(name)
+    if pet[:name] == name
+      pet_shop[:pets].delete(pet)
+    end
   end
 end
 
@@ -73,4 +74,36 @@ end
 
 def add_pet_to_customer(customer, new_pet)
   customer[:pets] << new_pet
+end
+
+def customer_can_afford_pet(customer, new_pet)
+  if customer[:cash] >= new_pet[:price]
+    return true
+  else
+    return false
+  end
+end
+
+def sell_pet_to_customer(pet_shop, pet, customer)
+  for pets in pet_shop[:pets]
+    if pets[:name] == pet
+      customer[:pets] << pets[:name]
+      pet_shop[:admin][:pets_sold] += 1
+      customer[:cash] -= pets[:price]
+      pet_shop[:admin][:total_cash] += pets[:price]
+    end
+
+    def customer_pet_count(customer)
+      customer[:pets].length
+    end
+    def pets_sold(pet_shop)
+      pet_shop[:admin][:pets_sold]
+    end
+    def customer_cash(customer)
+      customer[:cash]
+    end
+    def total_cash(pet_shop)
+      pet_shop[:admin][:total_cash]
+    end
+  end
 end
